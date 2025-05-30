a3b2210a8291199b9ffbce230495bfa8e07dddba
projects of hitz <hitesharma11111@gmail.com>
2025-05-30 13:03:30 +0530

    Remove Spring Boot projects from JAVA-Missions,coz of dupicate

--- JAVA-Missions/Spring-boot-projects/aryora-store/aryora-store/src/main/java/com/maryora/store/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.aryora.store.model;
-
-public enum UserRole {
-    ROLE_USER, // Regular customer
-    ROLE_PREMIUM, // Premium customer (with special benefits)
-    ROLE_ADMIN // Administrator
-}
